f279ea0a859b42ce1209d3bd94bc75302ed9a995
SumitGohil17 <118886308+SumitGohil17@users.noreply.github.com>
2024-08-01 23:03:03 +0530

    ```text refactor: Update login form in Login.js to include form submission handling
    The login form in the Login.js file has been updated to include form submission handling. The form now uses the onSubmit event to call the handleRegister function when the Register button is clicked. This change ensures that the form data is properly submitted and processed.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -113,7 +113,7 @@ function Login() {
                 <div className="w-full max-w-xs">
 
                     {register ? (<div className="mt-4">
-                        <form method="POST" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" >
+                        <form method="POST" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleRegister} >
                             {/* Register form fields */}
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -141,7 +141,7 @@ function Login() {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="******************" value={userRegister.password} onChange={handleChangeRegister} required />
                         </div>
                         <div className="flex items-center justify-between">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleRegister} >
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit' >
                                 Register
                             </button>
                         </div>
